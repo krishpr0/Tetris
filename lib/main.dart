3607e3bf38592910514e0581f73b06dcb9300a48
krishpr0 <krishnpradhan007@gmail.com>
2025-11-04 10:58:30 +0545

    Added the UI layout with the User Interaction. and also some logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,400 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const TetrisApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TetrisApp extends StatelessWidget {
+  const TetrisApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Tetirs',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TetrisGame(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class TetrisGame extends StatefulWidget {
+  const TetrisGame ({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<TetrisGame> createState() => _TetrisGameState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TetrisGameState extends  State<TetrisGame> {
+  final TetrisEngine _engine = TetrisEngine();
+  Timer? _gameTimer;
+  Timer? _fallTimer;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _startGame();
+
+  }
+
+  @override
+  void dispose() {
+    _gameTimer?.cancel();
+    _fallTimer?.cancel();
+    super.dispose();
+  }
+
+  void _startGame() {
+    _engine.reset();
+
+    _gameTimer = Timer.periodic(const Duration(milliseconds: 16), (timer) {
+      setState(() {
+        _engine.updateTime();
+      });
+    });
+
+    _fallTimer = Timer.periodic(const Duration(milliseconds: 500), (timer) {
+      if (!_engine.paused && !_engine.gameOver) {
+        setState(() {
+          _engine.movePiece(0, 1);
+        });
+      }
+    });
+  }
+
+  void _restartGame() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _engine.reset();
+    });
+  }
+
+  void _togglePause() {
+    setState(() {
+      _engine.togglePause();
     });
   }
 
   @override
+
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      backgroundColor: Colors.black,
+      body: SafeArea(
+          child: Padding(
+              padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                _buildHeader(),
+                const SizedBox(height: 10),
+                Expanded(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        _buildGameBoard(),
+                        const SizedBox(width: 10),
+                        _buildSidebar(),
+                      ],
+                    ),
+                ),
+                _buildControls(),
+              ],
+            ),
+          ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    );
+  }
+
+  Widget _buildHeader() {
+    final progress = _engine.progress;
+    return Column(
+      children: [
+        Text(
+          'Tetris',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 8),
+        Stack(
+          children: [
+            Container(
+              height: 20,
+              decoration: BoxDecoration(
+                color: Colors.grey[800],
+                borderRadius: BorderRadius.circular(10),
+              ),,
+            ),
+            AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                height: 20,
+              width: 20,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Colors.green, Colors.lightGreen],
+                ),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            Positioned.fill(
+              child: Center(
+                child: Text(
+                  '${(progress * 100).toStringAsFixed(1)}% Complete',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
             ),
           ],
         ),
+        const SizedBox(height: 4),
+        Text(
+          'Elasped: ${_engine.formattedTime}',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 14,
+          ),
+        ),
+      ],
+    );
+  }
+
+
+  Widget _buildGameBoard() {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.white54, width: 2),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      child: GridView.builder(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: TetrisEngine.gridWidth),
+      ),
+      itemCount: TetrisEngine.gridWidth * TetrisEngine.gridHeight,
+      itemBuilder: (context, index) {
+        final x = index % TetrisEngine.gridWidth;
+        final y = index ~/ TetrisEngine.gridWidth;
+        final Cell = _engine.getCell(x, y);
+
+        return Container(
+          margin: const EdgeInsets.all(0.6),
+          decoration: BoxDecoration(
+            color: cell.color,
+            border: cell.color != Colors.black ? Border.all(color: Colors.white30, width: 1) : null,
+          ),
+        );
+      },
+    ),
+    );
+  }
+
+
+  Widget _buildSidebar() {
+    return SizedBox(
+      width: 120,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildInfoCard('Score', _engine.score.toString()),
+          _buildInfoCard('Level', _engine.level.toString()),
+          _buildInfoCard('Lines', _engine.linesCleared.toString()),
+
+          const SizedBox(height: 20),
+
+          Text(
+            'Next',
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+          const SizedBox(height: 5),
+          _buildNextPiecePreview(),
+
+          const Spacer(),
+
+          if(_engine.gameOver)
+            _buildStatusCard('GAME OVER', Colors.red)
+          else if (_engine.paused)
+            _buildStatusCard('PAUSED', Colors.orange),
+
+          const SizedBox(height: 20),
+
+          _buildControlsInfo(),
+        ],
+      ),
+    );
+  }
+
+
+  Widget _buildInfoCard(String title, String value) {
+    return Card(
+      color: Colors.grey[900],
+      child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start ,
+            children: [
+              Text(
+                value,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ],
+          ),
+      ),
+    );
+  }
+
+
+  Widget _buildStatusCard(String text, Color color) {
+    return Card(
+      color: color,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+              fontWeight: FontWeight.bold
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+
+
+  Widget _buildNextPiecePreview() {
+    final nextPiece = _engine.nextPiece;
+    return Container(
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: Colors.grey[900],
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        children: nextPiece.shape.map((row) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: row.map((cell) {
+              return Container(
+                width: 12,
+                height: 12,
+                margin: const EdgeInsets.all(1),
+                decoration: BoxDecoration(
+                  color: cell == 1 ? nextPiece.color : Colors.transparent,
+                  border: cell == 1? Border.all(color: Colors.white30) : null,
+                ),
+              );
+            }).toList(),
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+
+  Widget _buildControlsInfo() {
+    return Card(
+      color: Colors.grey[900],
+      child: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Controls:',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(height: 4),
+              Text(
+                '<-_->: Move\n: Rotate\n: Soft Drop\n: Pause\n: Restart',
+                style: TextStyle(
+                  color: Colors.white60,
+                  fontSize: 10
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+
+    Widget _buildControls() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          //Left Button
+          _buildControlButton(Icons.arrow_left, () {
+            if (!_engine.paused && !_engine.gameOver) {
+              setState(() => _engine.movePiece(-1, 0));
+            }
+          }),
+
+          //Rotate Button
+          _buildControlButton(Icons.arrow_upward, () {
+            if (!_engine.paused && !_engine.gameOver) {
+              setState(() => _engine.rotatePiece());
+            }
+          }),
+
+          //Right Button
+          _buildControlButton(Icons.arrow_right, () {
+            if (!_engine.paused && !_engine.gameOver) {
+              setState(() => _engine.movePiece(1,0));
+            }
+          }),
+
+          //Down Button
+          _buildControlButton(Icons.arrow_downward, () {
+            if (!_engine.paused && !_engine.gameOver) {
+              setState(() => _engine.movePiece(0, 1));
+            }
+          }),
+
+          //Pause Button
+          _buildControlButton(_engine.paused ? Icons.play_arrow : Icons.pause, _togglePause,
+            color: _engine.paused ? Colors.green : Colors.orange,
+          ),
+
+          // Restart button
+          _buildControlButton(Icons.refresh, _restartGane, color: Colors.red),
+        ],
+      ),
+    );
+  }
+
+
+  Widget _buildControlButton(IconData icon, VoidCallback onPressed, {Color? color}) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color ?? Colors.blue,
+        shape: const CircleBorder(),
+        padding: const EdgeInsets.all(16),
+      ),
+      onPressed: onPressed,
+      child: Icon(icon, color: Colors.white),
     );
   }
 }
+
+
